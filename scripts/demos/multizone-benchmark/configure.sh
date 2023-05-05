a2c5804f5a53336c2630385466582a1e0314bde8
Jim Doty <jdoty@aerospike.com>
2023-05-05 08:46:31 -0600

    STASH FOR PRESENTATION

--- scripts/demos/multizone-benchmark/configure.sh
+++ scripts/demos/multizone-benchmark/configure.sh
@@ -1,29 +1,29 @@
 # absolute path to a valid features.conf file
-FEATURES_FILE="/Users/rglonek/aerolab/features.conf"
+FEATURES_FILE="/home/jdoty/src/github.com/citrusleaf/aerospike-server-enterprise/etc/features.conf"
 
 # if backend is 'docker', total nodes is NODES_PER_AZ*count(AWS_AVAILABILITY_ZONES), total clients is CLIENTS_PER_AZ*count(AWS_AVAILABILITY_ZONES)
-BACKEND="docker"
+BACKEND="aws"
 
 # aerospike version
 VER="6.2.0.6"
 
 # names
-CLUSTER_NAME="robert"
-AMS_NAME="glonek"
-CLIENT_NAME="hammertime"
+CLUSTER_NAME="demo"
+AMS_NAME="ams"
+CLIENT_NAME="clients"
 
 # region and list of AWS AZs to deploy in; also defines number of aerospike racks
 AWS_REGION="us-east-1"
-AWS_AVAILABILITY_ZONES=(us-east-1a us-east-1b us-east-1c)
+AWS_AVAILABILITY_ZONES=(us-east-1c us-east-1d us-east-1f)
 
 # number of server nodes and client machines per AZ (per rack)
 NODES_PER_AZ=2
 CLIENTS_PER_AZ=2
 
 # instances - cluster instance requires type with NVMe disks
-CLUSTER_AWS_INSTANCE="r5ad.4xlarge"
-AMS_AWS_INSTANCE="t3a.large"
-CLIENT_AWS_INSTANCE="t3a.large"
+CLUSTER_AWS_INSTANCE="r6id.8xlarge"
+AMS_AWS_INSTANCE="m6i.2xlarge"
+CLIENT_AWS_INSTANCE="c6in.8xlarge"
 
 # namespace name
 NAMESPACE="test"
@@ -38,8 +38,8 @@ AWS_PARTITIONS=25,25,25,25
 TEMPLATE="template.conf"
 
 # number of asbench per client instance/container - insert and read/update load
-asbench_per_instance_insert=2
-asbench_per_instance_load=2
+asbench_per_instance_insert=25
+asbench_per_instance_load=25
 
 # asbench details
 asbench_start_key=0
@@ -48,7 +48,7 @@ asbench_threads=16
 asbench_bin_name="testbin"
 asbench_ru_runtime=86400
 asbench_object="I1"
-asbench_ru_throughput=1000
+asbench_ru_throughput=10000
 asbench_ru_percent=80 # 80 means 80 percent reads, 20 percent writes
 asbench_socket_timeout=200
 asbench_total_timeout=1000
